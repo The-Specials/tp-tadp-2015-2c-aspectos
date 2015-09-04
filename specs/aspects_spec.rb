4368e1385e70ed782bfbd1b722b80298bc23d5b5
Matias Martino <matiasfmartino@gmail.com>
2015-09-04 12:33:51 -0300

    Adding tests

--- specs/aspects_spec.rb
+++ specs/aspects_spec.rb
@@ -4,8 +4,10 @@ require_relative 'data/origen_mocks'
 
 describe Aspects do
 
-  let(:origenValido) { Origen.new }
-  let(:origenVacio) { OrigenVacio.new }
+  let(:origenValido) { Object_Mock.new }
+  let(:origenInvalido) { OrigenInvalido.new }
+  let(:instance) { Object_Mock.new }
+  let(:regex) { Regex_Mock.new }
 
   describe '#on' do
 
@@ -17,25 +19,45 @@ describe Aspects do
 
     context 'when there are not any valid Origenes' do
       it do
-        expect{Aspects.on(origenVacio) do end}.to raise_error(ArgumentError, 'origen vacio')
+        expect{Aspects.on(origenInvalido) do end}.to raise_error(ArgumentError, 'origen vacio')
       end
 
       it do
-        expect{Aspects.on(origenVacio, OrigenVacio.new, OrigenVacio.new) do end}.to raise_error(ArgumentError, 'origen vacio')
+        expect{Aspects.on(origenInvalido, OrigenInvalido.new, OrigenInvalido.new) do end}.to raise_error(ArgumentError, 'origen vacio')
       end
     end
 
     context 'when there are at least one valid Origen' do
       it do
-        expect(Aspects.on(origenValido) do end).to eq [1]
+        expect(Aspects.on(origenValido) do end).to eq [origenValido]
       end
 
       it do
-        expect(Aspects.on(origenValido, origenVacio) do end).to eq [1]
+        expect(Aspects.on(origenValido, origenInvalido) do end).to eq [origenValido]
+      end
+    end
+
+    context 'when there are many valid Origenes' do
+      it do
+        expect(Aspects.on(instance, Class_Mock, Module_Mock) do end).to eq [instance, Class_Mock, Module_Mock]
+      end
+    end
+
+    context 'when there are valid and invalid Origenes' do
+      it do
+        expect(Aspects.on(origenInvalido, instance, OrigenInvalido.new, Module_Mock) do end).to eq [instance, Module_Mock]
+      end
+    end
+
+    context 'when there are duplicated Origenes' do
+      it do
+        expect(Aspects.on(origenValido, Class_Mock, instance, instance, Class_Mock) do end).to eq [origenValido, Class_Mock, instance]
+      end
+
+      it do
+        expect(Aspects.on(instance, Module_Mock, regex, Class_Mock) do end).to eq [instance, Module_Mock, Class_Mock]
       end
     end
 
   end
 end
-
-
--- specs/data/origen_mocks.rb
+++ specs/data/origen_mocks.rb
@@ -1,11 +1,29 @@
-class OrigenVacio
+class OrigenInvalido
   def getOrigenes
     nil
   end
 end
 
-class Origen
+class Object_Mock
   def getOrigenes
-    1
+    self
+  end
+end
+
+class Class_Mock
+  def self.getOrigenes
+    self
+  end
+end
+
+module Module_Mock
+  def self.getOrigenes
+    self
+  end
+end
+
+class Regex_Mock
+  def getOrigenes
+    [Module_Mock, Class_Mock]
   end
 end
