b611016449e987f60acf35aea595f372c8a97c90
BiancaPetracca <biancapetracca@gmail.com>
2015-09-08 20:54:48 -0300

    agrega tests

--- spec/aspect_spec.rb
+++ spec/aspect_spec.rb
@@ -33,4 +33,15 @@ describe 'Aspects test' do
       expect(Aspects.origen). to include(MiClase, Foolish)
     end
 
+    it 'me permite definir un origen a partir de una regex' do
+      Evil_class_666 = Class.new
+      Aspects.set_fuentes Evil_class_666, MiClase
+      Aspects.on /^E/, MiClase do end
+      expect(Aspects.origen). to include(Evil_class_666)
+    end
+
+  it 'probando matches_to_any' do
+   expect(/^E/.matches_to_any(Evil_class_666)).to include(Evil_class_666)
+  end
+
 end
