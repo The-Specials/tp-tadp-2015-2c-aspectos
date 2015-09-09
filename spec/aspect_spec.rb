5ef6b7035c8ea325ff1b882dc0153097cfbc96ec
BiancaPetracca <biancapetracca@gmail.com>
2015-09-09 00:28:26 -0300

    test fixes

--- spec/aspect_spec.rb
+++ spec/aspect_spec.rb
@@ -29,19 +29,15 @@ describe 'Aspects test' do
     it 'me permite definirle un origen' do
       Foolish = Class.new
       Aspects.set_fuentes Foolish, MiClase
-      Aspects.on(Foolish, MiClase) do end
-      expect(Aspects.origen). to include(MiClase, Foolish)
+      Aspects.on Foolish, MiClase, /^A/ do end
+      expect(Aspects.origen). to include MiClase, Foolish
     end
 
     it 'me permite definir un origen a partir de una regex' do
       Evil_class_666 = Class.new
       Aspects.set_fuentes Evil_class_666, MiClase
-      Aspects.on /^E/, MiClase do end
-      expect(Aspects.origen). to include(Evil_class_666)
+      Aspects.on /^E/, /^M/ do end
+      expect(Aspects.origen). to eq [Evil_class_666, MiClase]
     end
-
-  it 'probando matches_to_any' do
-   expect(/^E/.matches_to_any(Evil_class_666)).to include(Evil_class_666)
-  end
 
 end
