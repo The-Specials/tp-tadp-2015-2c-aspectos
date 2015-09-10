5dcb44f26ee8da6c214347e908127797bcb8f341
BiancaPetracca <biancapetracca@gmail.com>
2015-09-09 23:31:01 -0300

    agrega tests condicion

--- spec/aspect_spec.rb
+++ spec/aspect_spec.rb
@@ -3,38 +3,34 @@ require_relative '../src/aspects'
 
 describe 'Aspects test' do
 
-    it 'me permite definirle un origen con una clase' do
+    it 'Me permite definirle un origen' do
       MiClase = Class.new
-      Aspects.set_fuentes MiClase
-      Aspects.on MiClase do end
-      expect(Aspects.origen). to eq ([MiClase])
+      un_objeto = MiClase.new
+      Good_class_555 = Class.new
+      Evil_class_666 = Class.new
+      Super_evil_module = Module.new
+      Aspects.set_fuentes MiClase, Good_class_555, Evil_class_666, Super_evil_module
+      Aspects.on MiClase, /^G/, /^S/ do end
+      expect(Aspects.origen). to eq ([MiClase, Good_class_555, Super_evil_module])
     end
 
-    it 'si no le paso ningun origen tira error' do
+    it 'Si el origen es vacio tira error' do
       expect{Aspects.on}.to raise_error(ArgumentError)
     end
 
-    it 'si le paso un origen invalido tira error' do
+    it 'Si el origen es invalido tira error' do
       ClaseQueNoEstaEnFuente = Class.new
       Aspects.set_fuentes MiClase
       expect{Aspects.on ClaseQueNoEstaEnFuente}.to raise_error(ArgumentError)
     end
 
-    it 'si le paso una clase que no esta en la fuente y otra que si me lo permite' do
+    it 'Me permite definir un origen si un origen no esta en la fuente y otro si' do
       Aspects.set_fuentes MiClase
       Aspects.on(ClaseQueNoEstaEnFuente, MiClase)
       expect(Aspects.origen). to eq ([MiClase])
     end
 
-    it 'me permite definirle un origen' do
-      Foolish = Class.new
-      Aspects.set_fuentes Foolish, MiClase
-      Aspects.on Foolish, MiClase, /^A/ do end
-      expect(Aspects.origen). to include MiClase, Foolish
-    end
-
-    it 'me permite definir un origen a partir de una regex' do
-      Evil_class_666 = Class.new
+    it 'Me permite definir un origen a partir de una regex' do
       Aspects.set_fuentes Evil_class_666, MiClase
       Aspects.on /^E/, /^M/ do end
       expect(Aspects.origen). to eq [Evil_class_666, MiClase]
---
+++ spec/mega_evil.rb
@@ -0,0 +1,10 @@
+class Mega_evil
+
+  def evil_method
+
+  end
+
+  def nice_method
+
+  end
+end
