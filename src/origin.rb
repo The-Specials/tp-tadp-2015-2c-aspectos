4d774409ebef994403f647b595345838fffaa74a
Matias Martino <matiasfmartino@gmail.com>
2015-09-11 16:59:29 -0300

    Fixing bugs

--- src/origin.rb
+++ src/origin.rb
@@ -10,6 +10,10 @@ module Origen
   def origin_methods
     origin_method_names.map { |name| instance_method name }
   end
+
+  def origin_method name
+    origin_methods.detect{ |method| method.name.eql? name }
+  end
 end
 
 class Class
@@ -24,11 +28,19 @@ class Object
   include Origen
 
   def origin_method_names
-    singleton_class.instance_methods(true).concat(singleton_class.private_instance_methods(true))
+    methods(true).concat(private_methods(true))
   end
 
   def origin_methods
-    origin_method_names.map { |name| method name }
+    origin_method_names.map { |name| get_origin_method(name, self.singleton_class) }
+  end
+
+  private
+  def get_origin_method method, owner
+    origin_method = method(method)
+    origin_method.send :define_singleton_method, :owner, proc{ owner }
+
+    return origin_method
   end
 end
 
@@ -38,3 +50,19 @@ class Regexp
     valid_constants.map{ |c| Object.const_get(c) }
   end
 end
+
+#opcion para evitar efecto sobre el metodo en origin_methods
+# class OriginMethod < UnboundMethod
+#   def initialize original_method, eigen_class
+#     @method = original_method
+#     @eigen_class = eigen_class
+#   end
+#
+#   def owner
+#     @eigen_class
+#   end
+#
+#   def method_missing invoked_method, *args
+#     @method.send invoked_method, *args
+#   end
+# end
